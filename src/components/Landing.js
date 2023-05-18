11734d5df4723498f1ff31b2550aebbec2cea9b6
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-05-18 21:05:38 +0100

    CSS elements on App.css

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,12 @@
+import "/Users/emma/techpod_frontend/src/images/TechPodLogo.png";
+
 const Landing = () => {
   return (
     <div>
+      <img
+        src={require("/Users/emma/techpod_frontend/src/images/TechPodLogo.png")}
+        className="logo"
+      ></img>
       <h1>Welcome to TechPod</h1>
     </div>
   );
