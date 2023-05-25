9c0306e1889f4dc0f423db58a40f68168bf510ff
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-05-25 22:23:11 +0100

    added to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,19 @@
 const NavBar = () => {
-  return;
-  <nav className="nav">
-    <a href="/" className="site-title">
-      TechPod
-    </a>
-    <ul>
-      <li>
-        <a href="/about">About</a>
-        <a href="/community">Community</a>
-        <a href="/jobs">Jobs</a>
-        <a href="/events">Events</a>
-      </li>
-    </ul>
-  </nav>;
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        TechPod
+      </a>
+      <ul>
+        <li>
+          <a href="/about">About</a>
+          <a href="/community">Community</a>
+          <a href="/jobs">Jobs</a>
+          <a href="/events">Events</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
